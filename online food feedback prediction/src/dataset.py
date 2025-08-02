f8ae6938f4c844f24155d290f553692e0ba53587
Shahnawaz <shahnawazhusain556@gmail.com>
2025-08-02 23:40:42 +0530

    pipeline updated and column transformer added

--- online food feedback prediction/src/dataset.py
+++
@@ -1,7 +0,0 @@
-import pandas  as pd
-
-df = pd.read_csv("D:/online_Food_feedback_prediction/onlinefoods.csv")
-for col in df.columns:
-    print(f"Value counts for column '{col}':")
-    print(df[col].value_counts())
-    print('-' * 50)
